075e34ddc93828cd2077f50415ac046ba4998c8a
samanthainneo99 <43583835+samanthainneo99@users.noreply.github.com>
2022-01-18 15:00:29 -0500

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+# Samantha Inneo
+# SSW 567- Software Testing, Quality Assurance and Maintenance
+# Homework 00- Tools Setup
+# Description: Hello World Program to Show Successful Download 
+
+print("Hello World")
